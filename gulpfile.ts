9835c64a42addb405f8a757365f4257f46e2b071
zhouhoujun <freebirds8281@hotmail.com>
2017-07-03 19:55:40 +0800

    release v2

--- gulpfile.ts
+++ gulpfile.ts
@@ -2,12 +2,12 @@ import * as gulp from 'gulp';
 import { ITaskOption, Development } from 'development-tool';
 // import 'development-tool-node';
 
-Development.create(gulp, __dirname, {
-    tasks: <ITaskOption>{
+Development.create(gulp, __dirname, [
+    <ITaskOption>{
         src: 'src',
         dist: 'lib',
         buildDist: 'build',
         testSrc: 'test/**/*.spec.ts',
         loader: 'development-tool-node'
-    }
-});
+    }])
+    .start();
